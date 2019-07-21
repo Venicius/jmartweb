497132584fcf03412a6c14f2527f345d7b594dbd
Venicius Alves <venicius.alves@gmail.com>
2019-07-21 13:38:59 -0300

    210719

--- js/creative.js
+++ js/creative.js
@@ -56,3 +56,21 @@
   });
 
 })(jQuery); // End of use strict
+
+
+var token = '8050756577.63902cb.94c3037c42d246e98e8f89e6604ae0d1',
+//var token = '1362124742.7b33a8d.6613a3567f0a425f9852055b8ef743b7',
+  num_photos = 8,
+  container = document.getElementById( 'rudr_instafeed' ),
+  scrElement = document.createElement( 'script' );
+
+window.mishaProcessResult = function( data ) {
+for( x in data.data ){
+  container.innerHTML += '<div class="col col-md-4 col-sm-6 col-xs-6"> <a target="_blank" href="'+data.data[x].link+'"> <img class="img-responsive portifolio" width="250" height="250" src="'+data.data[x].images.standard_resolution.url+'"/></a></div>';
+  console.log(data.data[x]);
+  /*'<li><div class="col-lg-4" style="width: 18rem;"><img class="card-img-top" src="' + data.data[x].images.low_resolution.url + '"></li></div>';*/
+}
+}
+
+scrElement.setAttribute( 'src', 'https://api.instagram.com/v1/users/self/media/recent?access_token=' + token + '&count=' + num_photos + '&callback=mishaProcessResult' );
+document.body.appendChild( scrElement );
